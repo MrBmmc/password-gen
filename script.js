b6a17ecbe25ed59028fb4f8ca9c766a21fa758fb
Brian McMullen <clanmcmullen2012@gmail.com>
2021-02-18 00:41:40 -0500

    content errored, array conjunction modified... maybe fixed

--- script.js
+++ script.js
@@ -37,41 +37,41 @@ function generatePassword() {
     
     else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
 
-        choices = character = [...number, ...letter, ...upper];
+        choices = [...character, ...number, ...letter, ...upper];
     }
-    // Else if for 3 positive options
+    
     else if (confirmCharacter && confirmNumber && confirmUppercase) {
-        choices = character = [...number, ...upper];
+        choices = [...character, ...number, ...upper];
     }
     else if (confirmCharacter && confirmNumber && confirmLowercase) {
-        choices = character = [...number, ...letter];
+        choices = [ ...character, ...number, ...letter];
     }
     else if (confirmCharacter && confirmLowercase && confirmUppercase) {
-        choices = character = [...letter, ...upper];
+        choices = [ ...character, ...letter, ...upper];
     }
     else if (confirmNumber && confirmLowercase && confirmUppercase) {
-        choices = number = [...letter, ...upper];
+        choices = [ ...number, ...letter, ...upper];
     }
-    // Else if for 2 positive options 
+    
     else if (confirmCharacter && confirmNumber) {
-        choices = character = [number];
+        choices = [ ...character, ...number];
 
     } else if (confirmCharacter && confirmLowercase) {
-        choices = character = [letter];
+        choices = [ ...character, ...letter];
 
     } else if (confirmCharacter && confirmUppercase) {
-        choices = character = [upper];
+        choices = [ ...character, ...upper];
     }
     else if (confirmLowercase && confirmNumber) {
-        choices = letter = [number];
+        choices = [ ...letter, ...number];
 
     } else if (confirmLowercase && confirmUppercase) {
-        choices = letter = [upper];
+        choices = [...letter, ...upper];
 
     } else if (confirmNumber && confirmUppercase) {
-        choices = number = [upper];
+        choices = [ ...number, ...upper];
     }
-    // Else if for 1 positive option
+    
     else if (confirmCharacter) {
         choices = character;
     }
@@ -81,41 +81,25 @@ function generatePassword() {
     else if (confirmLowercase) {
         choices = letter;
     }
-    // Created space variable to fill uppercase conversion
+  
     else if (confirmUppercase) {
-        choices = upper = [upper];
+        choices = [ ...upper, ...upper];
     };
 
     
     var password = [];
-
-    // Start random selection variables:
-    // Random selection for all variables: 
+    
     for (var i = 0; i < enter; i++) {
         var pickChoices = choices[Math.floor(Math.random() * choices.length)];
         password.push(pickChoices);
     }
-    // This joins the password array and converts it to a string
-    // Worked with a tutor to incorporate this option
+    
     var button = password.join("");
     UserInput(button);
     return button;
 }
-// This puts the password value into the textbox
-// Changed function input to use textcontent
-function UserInput(ps) {
-    document.getElementById("password").textContent = ps;
+
+function UserInput(button) {
+    document.getElementById("password").textContent = button;
 }
 
-var copy = document.querySelector("#copy");
-copy.addEventListener("click", function () {
-    copyPassword();
-});
-// This copies the password value - works
-// Code example demonstrated in a youtube video: 
-// Source: https://youtu.be/9sT03jEwcaw
-function copyPassword() {
-    document.getElementById("password").select();
-    document.execCommand("Copy");
-    alert("Password copied to clipboard!");
-}
